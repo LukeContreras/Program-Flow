ac13a48df9c7f7a6924c7919496404332dfabf4b
Luke Contreras <lacontrer101@gmail.com>
2022-09-29 12:35:50 -0700

    Do While loops

--- whileloops.java
+++ whileloops.java
@@ -15,21 +15,21 @@ public class whileloops
     
      public void run()   {
         int i = 0;
-        while (i<15)    {
+        do    {
             System.out.println("i is " + i + " and less than 15"); 
             i = i + 2;
-        }
+        }while (i<15);
         int k = 10;
-        while (k>5)     {
+        do     {
             System.out.println("k is " + k + " and greater than 5");
             k = k - 2;
-        }
-        while (a<10 && b<=20)   {
+        }while (k>5);
+        do   {
             System.out.println("a is " + a + " and less than ten");
             System.out.println("b is " + b + " and less than or equal to 20");
             a = a + 1;
             b = b + 4;
-        }
+        }while (a<10 && b<=20)
     }
 
     public static void main(String[] args)
